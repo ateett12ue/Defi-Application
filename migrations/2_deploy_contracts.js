d920b496ccdd75085d585c03442d37afc2a920b0
ateet <ateet@propelld.com>
2021-08-20 00:40:06 +0530

    deployed Dapp token and Dai mock token to token farm

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,26 @@
 const _initial_migration = require("./1_initial_migration");
 
+const DappToken = artifacts.require("DappToken")
+const DaiToken = artifacts.require("DaiToken")
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+    // deploy dai token for address
+    await deployer.deploy(DaiToken);
+    const daiToken = await DaiToken.deployed()
+
+    // deploy Dapp token for address
+    await deployer.deploy(DappToken);
+    const dappToken = await DappToken.deployed()
+
+    // deploy Token Farm
+    await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+    const tokenFarm = await TokenFarm.deployed()
+
+    // transfer all dapp token to tokenfarm for yield interest
+    await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+    //transfering some dai to 3rd party account (invester) as after deployment it all will be ours if not transfered
+    await daiToken.transfer(accounts[1],'1000000000000000000000')
 };
 
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,21 @@
 pragma solidity ^0.5.0;
 
+
+//already deployed
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
-    // all data goes here
     string public name = "Dapp Token Farm";
+
+    DappToken public dappToken;
+    DaiToken public daiToken;
+    
+    //assign address to state variable
+    //DappToken is a type
+    constructor(DappToken _dappToken, DaiToken _daiToken) public
+    {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
