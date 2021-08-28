4071ac7672ffdfdfe95c18fcc8072dba3466e09f
ateet <ateet@propelld.com>
2021-08-29 01:19:47 +0530

    comment added

--- scripts/issue-tokens.js
+++ scripts/issue-tokens.js
@@ -1,6 +1,11 @@
 const TokenFarm = artifacts.require("TokenFarm")
 
+// comand to create file ==> mkdir scripts/ touch scripts/issue-tokens.js
+
+//truffle exec scripts/issue-tokens.js
 module.exports = async function(callback) {
+    let tokenFarm = await TokenFarm.deployed()
+    await tokenFarm.issueToken()
     console.log('token issued')
     callback()
 };
